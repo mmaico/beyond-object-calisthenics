0c053868f55bc7876b9689e7ea6617fb2f99e7eb
Marcelo Maico <peo_mjesus@uolinc.com>
2023-02-23 07:42:52 -0300

    Adicao e configuracao do application.properties e do data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO profiles (id, name) VALUES (1, 'teacher');
+INSERT INTO profiles (id, name) VALUES (2, 'director');
+INSERT INTO profiles (id, name) VALUES (3, 'studant');
+INSERT INTO profiles (id, name) VALUES (4, 'parents');
+
+INSERT INTO users (id, name, profile) VALUES (1, 'Jose', 3);
+INSERT INTO users (id, name, profile) VALUES (2, 'Maria', 3);
+INSERT INTO users (id, name, profile) VALUES (3, 'Joao', 2);
+INSERT INTO users (id, name, profile) VALUES (4, 'Joana', 1);
